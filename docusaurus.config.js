daa011a580bbdbe9a440f3dd51674b6430be6e07
jiangxinlei <shuyueqiuting@hotmail.com>
2022-12-31 01:07:19 +0800

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://jiangxinlei.github.io',
+  url: 'http://jiangxinlei.info',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
